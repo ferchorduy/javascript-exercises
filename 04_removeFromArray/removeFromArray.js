4eccc5c200033defca64d62eab0a7c93622e51a5
Fernando Orduy <53240305+ferchorduy@users.noreply.github.com>
2023-05-09 14:33:59 -0400

    Finish code and pass tests for 04 exercise with two for loops

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for (const removable of args) {
+        for (const element of array) {
+            if (removable === element) {
+                array.splice(array.indexOf(element), 1);
+            } else {
+                continue;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
