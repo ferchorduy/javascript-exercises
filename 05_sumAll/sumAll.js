b64ff49a97790d2f4a972e4171b31abf1d02ff24
Fernando Orduy <53240305+ferchorduy@users.noreply.github.com>
2023-05-09 17:07:11 -0400

    Finish code and pass tests for 05 JS exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let finalSum = 0;
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return "ERROR";
+    } else if (num1 >= 0 && num2 >= 0) {
+        for (let i = Math.min(num1, num2); i < (Math.max(num1, num2) + 1); i++) {
+            finalSum += i;
+        }
+    } else {
+        return "ERROR";
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
