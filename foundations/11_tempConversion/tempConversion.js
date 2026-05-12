750413beee9e6df6f2be4147b796b3203b952405
ferchorduy <53240305+ferchorduy@users.noreply.github.com>
2026-05-12 14:44:01 -0400

    Finish 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+    let celsius = ((fahrenheit - 32) / (1.8));
+    return Number(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = ((celsius * 1.8) + 32);
+  return Number(fahrenheit.toFixed(1));
 };
+
+// const convertToCelsius = f => Number(((f - 32) / 1.8).toFixed(1))
+// const convertToFahrenheit = c => Number(((c * 1.8) + 32).toFixed(1)) // other solution, inline
 
 // Do not edit below this line
 module.exports = {
