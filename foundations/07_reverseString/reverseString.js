be42c2d401bfe4f95a4becdae492099cb3361d45
ferchorduy <53240305+ferchorduy@users.noreply.github.com>
2026-05-12 05:50:36 -0400

    Finish reverseString
    Have to learn to use methods to my advantage. Although, it is fun making
    my own algorithms.

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let newString = '';
+  for (let i = str.length-1 ; i >= 0 ; i--) {
+    // newString += str.at(-1); // my attempt, Claude tried another way, below
+    // str = str.slice(0, i);
+    newString += str[i];
+  } return newString;
 };
+
+// const reverseString = str => [...str].reverse().join('') // Claude code, same as answer except used [...str] as opposed to split()
 
 // Do not edit below this line
 module.exports = reverseString;
