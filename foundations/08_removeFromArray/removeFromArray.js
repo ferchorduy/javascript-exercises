3d3f4537a2773aaa6645767920a274d92da96068
ferchorduy <53240305+ferchorduy@users.noreply.github.com>
2026-05-12 08:59:57 -0400

    Finish 08_removeFromArray
    Learn how to use methods and not make an algorithm from scratch.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...rest) {
+  for (const value of rest) {
+    for (let i = arr.length-1 ; i >= 0; i--) {
+      if (arr[i] === value) arr.splice(i, 1);
+    } 
+  } return arr;
 };
+
+// const removeFromArray = (arr, ...rest) => arr.filter(item => !rest.includes(item)) // one liner option
 
 // Do not edit below this line
 module.exports = removeFromArray;
