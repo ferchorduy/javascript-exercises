9f0672963f6629eb54d353afe9c04a24b58f7352
ferchorduy <53240305+ferchorduy@users.noreply.github.com>
2026-05-12 14:18:55 -0400

    Finish 09_sumAll
    Finished exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+  let arr = [];
+  let c = a;
+  if (typeof a !== 'number' || typeof b !== 'number') return 'ERROR';
+  if (a < 0 || b < 0) return 'ERROR';
+  if (Number.isInteger(a) === false || Number.isInteger(b) === false) return 'ERROR'; // can do !Number.isInteger(a/b)
+  if (b < a) a = b, b = c;                              // use Math.min() and Math.max() to get min and max between a and b
+  for (i = a; i <= b; i++) {
+    arr.push(i);                                        // can make a sum var and just add all numbers in arr, rather than newArr and reduce
+  } return arr.reduce((acc, curr) => acc + curr, 0);
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
